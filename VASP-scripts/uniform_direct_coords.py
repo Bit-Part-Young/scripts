f66def578c9666f21388ce42240eeabe691f75c7
yangsl <3242809681@qq.com>
2024-06-03 11:58:33 +0800

    update

--- VASP-scripts/uniform_direct_coords.py
+++ VASP-scripts/uniform_direct_coords.py
@@ -1,20 +1,24 @@
-"""将 VASP POSCAR 中原子坐标分数范围限制在 0-1 之间"""
+"""将 VASP POSCAR Direct 坐标范围限制在 0-1 之间"""
 
 import sys
 
-from ase.io.vasp import read_vasp, write_vasp
+from ase.io import read, write
 
 
 def uniform_direct_coords(filename: str):
-    """将 VASP POSCAR 中原子坐标分数范围限制在 0-1 之间"""
+    """将 VASP POSCAR Direct 坐标范围限制在 0-1 之间"""
 
-    atoms = read_vasp(filename)
-    # scale_positions 坐标值范围在 0-1 之间
+    atoms = read(filename)
     scale_positions = atoms.get_scaled_positions()
-
     atoms.set_scaled_positions(scale_positions)
 
-    write_vasp(filename, atoms, direct=True, sort=False)
+    write(
+        filename,
+        images=atoms,
+        format="vasp",
+        direct=True,
+        sort=False,
+    )
 
     print("The direct coordinates range have been uniformed to 0-1.")
 
