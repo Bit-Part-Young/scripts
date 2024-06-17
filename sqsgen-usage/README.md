892bb46ad683eaa3cf94442a818f83c4ee1a9438
yangsl <3242809681@qq.com>
2024-06-17 16:00:17 +0800

    add sqsgen scripts

---
+++ sqsgen-usage/README.md
@@ -0,0 +1,5 @@
+# sqsgen 使用
+
+- `sqs.yaml`, `sqs_B2.yaml`, `sqs_TiAlBN.yaml`: SQS 参数设置（示例）
+- `run.sh`: 运行 sqsgen 
+
---
+++ sqsgen-usage/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sqsgen run iteration sqs.yaml -di parameters -di objective -nm -e -f vasp -c gz -w ase
+
