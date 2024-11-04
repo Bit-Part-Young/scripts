2e8fd0a95d40c7ac040b35b65669e7c2cd923a7c
yangsl <3242809681@qq.com>
2024-11-04 23:42:06 +0800

    add get_vasp_data.py

---
+++ VASP-scripts/get_vasp_data.py
@@ -0,0 +1,36 @@
+"""获取 VASP 计算目录的数据"""
+
+import sys
+
+from atomate.vasp.drones import VaspDrone
+from pymatgen.core.structure import Structure
+
+
+def get_vasp_data(path: str) -> dict:
+    """获取 VASP 计算目录的数据"""
+
+    drone = VaspDrone()
+    document = drone.assimilate(path=path)
+
+    energy = document["output"]["energy"]
+
+    structure_dict = document["output"]["structure"]
+    structure = Structure.from_dict(structure_dict)
+    lattice = structure.lattice.abc
+
+    data_dict = {
+        "energy": energy,
+        "a": lattice[0],
+        "b": lattice[1],
+        "c": lattice[2],
+    }
+
+    print(data_dict)
+
+    return data_dict
+
+
+if __name__ == "__main__":
+    path = sys.argv[1]
+
+    get_vasp_data(path)
