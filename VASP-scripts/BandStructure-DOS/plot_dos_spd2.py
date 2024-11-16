2d7c68bad1bc1a3364b83cbeb006ec96bbe72bf3
yangsl <3242809681@qq.com>
2024-11-16 17:09:09 +0800

    update plot_dos_spd2.py

--- VASP-scripts/BandStructure-DOS/plot_dos_spd2.py
+++ VASP-scripts/BandStructure-DOS/plot_dos_spd2.py
@@ -5,29 +5,64 @@ import pandas as pd
 from scipy.ndimage import gaussian_filter1d
 from spt.plot_params import set_roman_plot_params
 
-csv_fname = "dos_spd.csv"
-df = pd.read_csv(csv_fname)
-energy = df["energy"]
 
-set_roman_plot_params()
-fig, ax = plt.subplots()
+def plot_dos_spd(
+    filter: bool,
+    sigma: float,
+    figname: str,
+):
 
-for i in range(1, df.shape[1]):
-    # 平滑处理
-    # dos_smooth = gaussian_filter1d(df.iloc[:, i], sigma=0.05)
-    # ax.plot(energy, dos_smooth, label=df.columns[i])
-    ax.plot(energy, df.iloc[:, i], label=df.columns[i])
+    csv_fname = "dos_spd.csv"
+    df = pd.read_csv(csv_fname)
+    energy = df["energy"]
 
-ax.axvline(x=0, color="black", linestyle="--")
+    set_roman_plot_params()
+    fig, ax = plt.subplots()
 
-ax.set_xlim(-10, 2.5)
-ax.set_ylim(0, 35)
+    for i in range(1, df.shape[1]):
+        if filter:
+            # 平滑处理
+            dos_smooth = gaussian_filter1d(df.iloc[:, i], sigma=sigma)
+            ax.plot(
+                energy,
+                dos_smooth,
+                label=df.columns[i],
+            )
+        else:
+            ax.plot(
+                energy,
+                df.iloc[:, i],
+                label=df.columns[i],
+            )
 
-ax.set_xlabel("Energy (eV)")
-ax.set_ylabel("DOS")
+    ax.axvline(
+        x=0,
+        color="black",
+        linestyle="--",
+    )
 
-ax.legend()
+    ax.set(
+        xlim=(-10, 2.5),
+        ylim=(0, 35),
+        xlabel="Energy (eV)",
+        ylabel="DOS",
+    )
 
-fig.savefig("dos_spd.png")
+    ax.legend(handlelength=1.0)
 
-print("DOS Figure is generated.")
+    fig.savefig(figname)
+
+    print("DOS Figure is generated.")
+
+
+if __name__ == "__main__":
+    plot_dos_spd(
+        filter=False,
+        figname="dos_spd_raw.png",
+    )
+
+    plot_dos_spd(
+        filter=True,
+        sigma=1.0,
+        figname="dos_spd.png",
+    )
