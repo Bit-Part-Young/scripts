9cdff10a1c6040fc3bc412920f178844b082d852
yangsl <3242809681@qq.com>
2024-12-16 11:32:24 +0800

    add get_mpid_data.py

---
+++ pymatgen-usage/get_mpid_data.py
@@ -0,0 +1,56 @@
+"""根据 Material Project material id 获取其数据"""
+
+from pprint import pprint
+
+from mp_api.client import MPRester
+from pymatgen.core import SETTINGS
+
+
+def get_mpid_data(
+    material_id: str,
+):
+    """根据 Material Project material id 获取其数据"""
+
+    PMG_API_KEY = SETTINGS.get("PMG_MAPI_KEY")
+
+    fields = [
+        "material_id",
+        "structure",
+        "formula_pretty",
+        "symmetry",
+        "nsites",
+        "energy_per_atom",
+        "formation_energy_per_atom",
+        "energy_above_hull",
+        "is_stable",
+    ]
+
+    with MPRester(PMG_API_KEY) as mpr:
+        docs = mpr.materials.summary.search(
+            fields=fields,
+            material_ids=material_id,
+        )
+
+    docs = docs[0]
+
+    print(f"\n{material_id} data info:")
+    data_dict = {
+        "material_id": str(docs.material_id),
+        "formula": docs.formula_pretty,
+        "crystal_system": str(docs.symmetry.crystal_system),
+        "spacegroup": docs.symmetry.symbol,
+        "nsites": docs.nsites,
+        "energy_per_atom": docs.energy_per_atom,
+        "formation_energy_per_atom,": docs.formation_energy_per_atom,
+        "energy_above_hull": docs.energy_above_hull,
+        "is_stable": docs.is_stable,
+    }
+
+    return data_dict
+
+
+if __name__ == "__main__":
+    mpid = "mp-131"
+    data = get_mpid_data(mpid)
+
+    pprint(data)
