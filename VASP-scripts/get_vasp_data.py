5b8815f5469d776f7c54113f8c5f94e3c94ce90d
BPY <3242809681@qq.com>
2024-12-27 10:04:31 +0800

    update get VASP data scripts

--- VASP-scripts/get_vasp_data.py
+++ VASP-scripts/get_vasp_data.py
@@ -9,6 +9,15 @@ from atomate.vasp.drones import VaspDrone
 from pymatgen.core.structure import Structure
 
 
+def seconds_to_hms(seconds: float):
+    """将秒数转换为时分秒格式"""
+
+    hours, remainder = divmod(seconds, 3600)
+    minutes, seconds_new = divmod(remainder, 60)
+
+    return f"{int(hours)}h-{int(minutes)}min-{int(seconds_new)}s"
+
+
 def get_vasp_data(path: str = ".") -> dict:
     """获取 VASP 计算目录输出数据"""
 
@@ -16,16 +25,21 @@ def get_vasp_data(path: str = ".") -> dict:
     document = drone.assimilate(path=path)
 
     energy = document["output"]["energy"]
+    natoms = document["nsites"]
+    time_cost = document["run_stats"]["overall"]["Total CPU time used (sec)"]
+    time_cost = seconds_to_hms(time_cost)
 
     structure_dict = document["output"]["structure"]
     structure = Structure.from_dict(structure_dict)
     lattice = structure.lattice.abc
 
     data_dict = {
-        "energy": energy,
         "a": lattice[0],
         "b": lattice[1],
         "c": lattice[2],
+        "natoms": natoms,
+        "energy": energy,
+        "time_cost": time_cost,
     }
 
     df = pd.DataFrame(data_dict, index=[0]).round(5)
--- VASP-scripts/get_vasp_data2.py
+++ VASP-scripts/get_vasp_data2.py
@@ -6,23 +6,38 @@ import argparse
 import os
 
 import pandas as pd
-from pymatgen.io.vasp.outputs import Vasprun
+from pymatgen.io.vasp.outputs import Outcar, Vasprun
+
+
+def seconds_to_hms(seconds: float):
+    """将秒数转换为时分秒格式"""
+
+    hours, remainder = divmod(seconds, 3600)
+    minutes, seconds_new = divmod(remainder, 60)
+
+    return f"{int(hours)}h-{int(minutes)}min-{int(seconds_new)}s"
 
 
 def get_vasp_data(path: str = "."):
     """获取 VASP 计算目录输出数据"""
 
     vasprun_path = os.path.join(path, "vasprun.xml")
+    outcar_path = os.path.join(path, "OUTCAR")
 
     vasprun = Vasprun(vasprun_path)
+    outcar = Outcar(outcar_path)
 
     structure = vasprun.final_structure
     natoms = structure.num_sites
+
+    time_cost = outcar.run_stats["Total CPU time used (sec)"]
+    time_cost = seconds_to_hms(time_cost)
 
     data_dict = {
         "energy": vasprun.final_energy,
         "natoms": natoms,
         "converged": vasprun.converged,
+        "time_cost": time_cost,
     }
 
     df = pd.DataFrame(data_dict, index=[0]).round(5)
