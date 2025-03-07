b61601cc5489ac0117228f9674d7031877a0808a
YSL <3242809681@qq.com>
2025-03-07 17:10:08 +0800

    add get_virial.sh

---
+++ MTP/get_virial.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 获取 OUTCAR、train.cfg 文件中的 Stress/Virial 信息
+
+if [[ -f "OUTCAR" ]]; then
+  grep -A2 'FORCE on cell' OUTCAR | awk 'NR <= 3'
+  grep -A20 'FORCE on cell' OUTCAR | grep 'Total'
+  echo
+  grep -A2 'FORCE on cell' OUTCAR | awk 'NR <= 3'
+  grep -A20 'FORCE on cell' OUTCAR | grep 'in kB'
+fi
+
+if [[ -f "train.cfg" ]]; then
+  grep -A1 'PlusStress' train.cfg | awk 'NR == 1 || NR % 3 == 2'
+fi
