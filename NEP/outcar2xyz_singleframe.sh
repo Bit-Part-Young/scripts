c07918c7697782f6625c3527fec5d0f695eb1731
YSL <3242809681@qq.com>
2025-04-15 16:22:18 +0800

    update

--- NEP/outcar2xyz_singleframe.sh
+++ NEP/outcar2xyz_singleframe.sh
@@ -33,13 +33,13 @@ outcar2xyz_singleframe(){
 
   # 能量
   # energy=$(grep "free  energy   TOTEN" ${outcar_fn} | tail -1 | awk '{printf "%.7f\n", $5}')
-  energy=$(grep "energy  without entropy" ${outcar_fn} | tail -1 | awk '{printf "%.7f\n", $7}')
+  energy=$(grep "energy  without entropy" ${outcar_fn} | tail -1 | awk '{print $7}')
 
   # virial 数据
   virial=$(grep -A 20 "FORCE on cell =-STRESS" ${outcar_fn} | grep "Total " | tail -1 | awk '{print $2,$5,$7,$5,$3,$6,$7,$6,$4}')
 
   # 添加多个标签实现
-  label_array=("element" "group" "tag")
+  label_array=("element" "group" "description" "tag")
   if [[ $# -eq 0 || $# -eq 3 ]]; then
     echo "config_type=\"${config_type}\" pbc=\"T T T\" energy=${energy} Lattice=\"${lattice}\" virial=\"${virial}\" Properties=species:S:1:pos:R:3:forces:R:3" >> $xyz_fn
   elif [[ $# -gt 3 ]]; then
@@ -87,13 +87,14 @@ get_help(){
   echo "    outcar_fn           OUTCAR filename, default OUTCAR"
   echo "    xyz_fn              xyz filename, default NEP-dataset.xyz"
   echo "    config_type         config type tag, default 'outcar2xyz'"
-  echo "    addtional_labels    additional labels, default element group tag"
+  echo "    addtional_labels    additional labels, default element group description tag"
 
   echo -e "\nExamples:"
   echo "    $script_name"
   echo "    $script_name OUTCAR NEP-dataset.xyz 'outcar2xyz'"
   echo "    $script_name OUTCAR NEP-dataset.xyz 'outcar2xyz' 'Nb'"
-  echo "    $script_name OUTCAR NEP-dataset.xyz 'outcar2xyz' 'Nb' 'perturbation' 'train'"
+  echo "    $script_name OUTCAR NEP-dataset.xyz 'outcar2xyz' 'Nb' 'perturbation' 'configuration 1' 'train'"
+  echo "    $script_name OUTCAR NEP-dataset.xyz 'outcar2xyz' 'Nb' 'NVT-AIMD' 'snapshot 1' 'train'"
   exit 0
 }
 
