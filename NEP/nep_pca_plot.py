16d8fb6ced8b0f4ed0f337d038317fd91eaf1651
YSL <3242809681@qq.com>
2025-04-16 23:29:36 +0800

    update NEP scripts

--- NEP/nep_pca_plot.py
+++ NEP/nep_pca_plot.py
@@ -1,98 +1,84 @@
-"""NEP 未归一化 & 归一化 描述符 PCA 2 维绘制
+#!/usr/bin/env python3
 
-测试 8762 frames, 509917 atoms 的构型训练集 xyz 文件
-Master 运行耗时 ~4 min（其中 描述符获取、处理耗时约 ~2 min，PCA 绘图耗时约 ~2 min）
-"""
+"""NEP 描述符（未归一化） PCA 2 维绘制"""
 
 # [ ] 是否可进一步优化以节约耗时
 
+import argparse
 import os
 
 import matplotlib.pyplot as plt
 import numpy as np
 from ase.io import read
 from calorine.nep import get_descriptors
-from matplotlib.axes import Axes
 from sklearn.decomposition import PCA
-from spt.plot_params import set_roman_plot_params
 
-# np.set_printoptions(suppress=True, precision=10)
+np.set_printoptions(suppress=True, precision=10)
 
 
 def nep_pca_plot(
-    all_descriptors_fn: str = "all_descriptors.npy",
-    all_normalized_descriptors_fn: str = "all_normalized_descriptors.npy",
+    descriptors_fn: str = "descriptors.npy",
+    output_fn: str = "pca.png",
 ):
-    """NEP 未归一化 & 归一化 描述符 PCA 2 维绘制"""
+    """NEP 描述符（未归一化） PCA 2 维绘制"""
 
-    all_descriptors = np.load(all_descriptors_fn)
-    all_normalized_descriptors = np.load(all_normalized_descriptors_fn)
+    descriptors = np.load(descriptors_fn)
 
-    set_roman_plot_params()
-    fig, axes = plt.subplots(ncols=2, figsize=(12, 6))
+    fig, ax = plt.subplots()
 
-    for i, ax in enumerate(axes):
-        ax: Axes
-        pca = PCA(n_components=2)
-        if i == 0:
-            pc = pca.fit_transform(all_descriptors)
-            title = "Unnormalized"
-        else:
-            pc = pca.fit_transform(all_normalized_descriptors)
-            title = "Normalized"
+    pca = PCA(n_components=2)
+    pc = pca.fit_transform(descriptors)
 
-        ax.scatter(pc[:, 0], pc[:, 1], alpha=0.5, label="structures")
+    ax.scatter(
+        pc[:, 0],
+        pc[:, 1],
+        alpha=0.5,
+        s=10,
+        label="structures",
+    )
 
-        ax.set_title(title)
-        ax.set_xlabel(f"PAC dim0 - Var={pca.explained_variance_ratio_[0]:.2f}")
-        ax.set_ylabel(f"PAC dim1 - Var={pca.explained_variance_ratio_[1]:.2f}")
+    ax.set_xlabel(f"PAC dim0 - Var={pca.explained_variance_ratio_[0]:.2f}")
+    ax.set_ylabel(f"PAC dim1 - Var={pca.explained_variance_ratio_[1]:.2f}")
 
-        # ax.legend(frameon=False)
+    fig.savefig(output_fn)
 
-    plt.tight_layout()
-    fig.align_ylabels()
-
-    fig.savefig("pca_plot.png")
-
-    print("PCA 2D unnormalized & normalized figure generated!")
+    print("\nNEP descriptors PCA 2D figure generated!")
 
 
 def get_nep_descriptors(
     xyz_fn: str = "train.xyz",
     model_fn: str = "nep.txt",
+    descriptors_fn: str = "descriptors.npy",
 ):
-    """获取 NEP 未归一化 & 归一化 描述符"""
+    """获取 NEP 描述符（未归一化）"""
 
     atoms_list = read(xyz_fn, index=":", format="extxyz")
 
-    descriptors = []
+    descriptors_list = []
     for atoms in atoms_list:
-        descriptors.append(get_descriptors(structure=atoms, model_filename=model_fn))
+        descriptors_list.append(
+            get_descriptors(structure=atoms, model_filename=model_fn)
+        )
 
     # 行合并所有描述符
-    all_descriptors = np.concatenate(descriptors, axis=0)
+    descriptors_array = np.concatenate(descriptors_list, axis=0)
 
-    print(f"Total number of atoms in dataset: {all_descriptors.shape[0]}")
-    print(f"Number of descriptor components:  {all_descriptors.shape[1]}")
-
-    # 描述符归一化
-    descriptor_mean = all_descriptors.mean(axis=0)
-    descriptor_std = all_descriptors.std(axis=0)
-    normalized_descriptors = [(d - descriptor_mean) / descriptor_std for d in descriptors]
-    all_normalized_descriptors = np.concatenate(normalized_descriptors, axis=0)
+    print(f"Total number of atoms in dataset: {descriptors_array.shape[0]}")
+    print(f"Number of descriptor components:  {descriptors_array.shape[1]}")
 
     # 数据集很大时，建议保存成文件
-    np.save("all_descriptors.npy", all_descriptors)
-    np.save("all_normalized_descriptors.npy", all_normalized_descriptors)
+    np.save(descriptors_fn, descriptors_array)
 
 
 def main():
+    descriptors_fn = "descriptors.npy"
+    if not os.path.exists(descriptors_fn):
+        get_nep_descriptors(descriptors_fn=descriptors_fn)
 
-    if not os.path.exists("all_descriptors.npy"):
-        get_nep_descriptors()
-
-    nep_pca_plot()
+    nep_pca_plot(descriptors_fn=descriptors_fn)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser("NEP descriptors PCA 2D plot.")
+
     main()
---
+++ NEP/perturb_dpdata.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+"""使用 dpdata 生成随机 cell & position 扰动结构"""
+
+import argparse
+
+import dpdata
+from ase.io import write
+
+
+def perturb_dpdata(
+    input_fn: str = "POSCAR",
+    pert_num: int = 50,
+    cell_pert_fraction: float = 0.10,
+    atom_pert_distance: float = 0.1,
+    output_fn: str = "perturbed.xyz",
+):
+    """使用 dpdata 生成随机 cell & position 扰动结构"""
+
+    perturbed_system = dpdata.System(input_fn, fmt="vasp/poscar").perturb(
+        pert_num=pert_num,
+        cell_pert_fraction=cell_pert_fraction,
+        atom_pert_distance=atom_pert_distance,
+        atom_pert_style="uniform",
+        atom_pert_prob=1.0,
+    )
+
+    atoms_list = perturbed_system.to("ase/structure")
+    print(len(atoms_list))
+    write(output_fn, atoms_list, format="extxyz", append=True)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Generate random cell & position perturbed structures using dpdata.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        epilog="Author: SLY.",
+    )
+
+    parser.add_argument(
+        "input_fn",
+        type=str,
+        default="POSCAR",
+        help="Input structure filename",
+    )
+    parser.add_argument(
+        "output_fn",
+        type=str,
+        default="perturbed.xyz",
+        help="Output filename",
+    )
+
+    parser.add_argument(
+        "-pn",
+        "--pert_num",
+        type=int,
+        default=50,
+        help="Number of perturbed structures",
+    )
+    parser.add_argument(
+        "-cp",
+        "--cell_perturb",
+        type=float,
+        default=0.10,
+        help="Cell perturbation fraction",
+    )
+    parser.add_argument(
+        "-ap",
+        "--atom_perturb",
+        type=float,
+        default=0.1,
+        help="Atom perturbation distance magnitude",
+    )
+
+    args = parser.parse_args()
+
+    perturb_dpdata(
+        input_fn=args.input_fn,
+        output_fn=args.output_fn,
+        pert_num=args.pert_num,
+        cell_pert_fraction=args.cell_perturb,
+        atom_pert_distance=args.atom_perturb,
+    )
