257b938b5b4002adf528372b16f07b17583b5e8d
YSL <3242809681@qq.com>
2025-06-04 16:05:11 +0800

    add & update psp generation scripts

--- VASP-scripts/get_psp2.py
+++ VASP-scripts/get_psp2.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
-"""生成 VASP、pymatgen 推荐的 PBE 赝势 POTCAR 文件"""
+"""生成 VASP、pymatgen 推荐的 PBE 赝势 POTCAR 文件（不依赖 pymatgen，速度更快）"""
 
 import argparse
 import os
-
-from pymatgen.io.vasp.inputs import Poscar, Potcar
+import subprocess
 
 # Data source: https://github.com/materialsproject/pymatgen/blob/master/src/pymatgen/io/vasp/MPRelaxSet.yaml
 pbe_dict_pymatgen = {
@@ -90,6 +89,17 @@ pbe_dict_vasp = {
 }
 
 
+def parse_poscar_file(structure_fn: str = "POSCAR") -> list:
+    """解析 poscar 文件"""
+
+    with open(structure_fn, "r") as file:
+        lines = file.readlines()
+
+    element_symbols = lines[5].strip().split()
+
+    return element_symbols
+
+
 def get_psp(
     structure_fn: str = "POSCAR",
     psp_recommended: str = "vasp",
@@ -99,8 +109,11 @@ def get_psp(
     if os.path.exists("POTCAR"):
         os.remove("POTCAR")
 
-    poscar = Poscar.from_file(structure_fn)
-    element_symbols = poscar.site_symbols
+    psp_path_root = os.getenv("VASP_PSP_PATH")
+    functional_subdir = "PAW_PBE"
+    psp_path = os.path.join(psp_path_root, functional_subdir)
+
+    element_symbols = parse_poscar_file(structure_fn)
 
     if psp_recommended == "vasp":
         psp_dict = pbe_dict_vasp
@@ -113,18 +126,17 @@ def get_psp(
 
     psp_symbols = [psp_dict[element] for element in element_symbols]
 
-    # 这里的 symbols 是元素赝势符号，而非元素符号
-    potcar = Potcar(
-        symbols=psp_symbols,
-        functional="PBE",
-    )
-    potcar.write_file("POTCAR")
+    for psp_symbol in psp_symbols:
+        element_psp_path = os.path.join(psp_path, psp_symbol)
+        element_psp_fn = os.path.join(element_psp_path, "POTCAR")
+
+        subprocess.run(f"cat {element_psp_fn} >> POTCAR", shell=True)
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(
-        description="Generate VASP, pymatgen recommended PBE pseudopotentials.",
+        description="Generate VASP, pymatgen recommended PBE pseudopotentials (pymatgen independent, faster).",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         epilog="Author: SLY.",
         allow_abbrev=True,
