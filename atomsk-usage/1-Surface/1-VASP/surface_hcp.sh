48ed95c0d9f3044ada6c6043870b0a8b0d5adc59
BPY <3242809681@qq.com>
2025-08-08 09:59:12 +0800

    add hcp pyramidal I and II type surface

--- atomsk-usage/1-Surface/1-VASP/surface_hcp.sh
+++ atomsk-usage/1-Surface/1-VASP/surface_hcp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# HCP Basal / (001), Prismatic / (100) 表面模型构建
+# HCP Basal / {0001}, Prismatic / {11-20} , PyramidalI / {10-11}, PyramidalII / {11-22} 表面模型构建
 
 
 surface_hcp() {
@@ -11,20 +11,18 @@ surface_hcp() {
   dup_x="${dup_x:-1}"
   dup_y="${dup_y:-1}"
   num_layer="${num_layer:-12}"
-  vacuum="${vacuum:-15}"
+  vacuum="${vacuum:-15.0}"
   vacuum_half=$(awk "BEGIN {print ${vacuum}/2}")
 
 
-  # Basal / (001) 表面
+  #------------------------- Basal / (0001) 表面 ---------------------------
+  # 默认扩胞 1x1x6
   dup_z=$(awk "BEGIN {print ${num_layer}/2}")
   atomsk --create hcp ${a} ${c} ${symbol} \
     orient "[11-20]" "[-1100]" "[0001]" \
     -duplicate ${dup_x} ${dup_y} ${dup_z} \
-    -fix x -fix y \
-    -cell add ${vacuum} z \
-    -shift 0 0 ${vacuum_half} \
-    -sort species pack \
-    -fractional vasp
+    -cell add ${vacuum} z -shift 0 0 ${vacuum_half} \
+    -sort species pack -fractional vasp
 
   echo -e "\n"
   printf "%`tput cols`s" | tr ' ' '#'
@@ -33,31 +31,63 @@ surface_hcp() {
   mv POSCAR POSCAR.basal
 
 
-  # Prismatic / (100) 表面
+  #------------------------- Prismatic / {11-20} 表面 ---------------------------
+  # 默认扩胞 1x1x3
   dup_z=$(awk "BEGIN {print ${num_layer}/4}")
   atomsk --create hcp ${a} ${c} ${symbol} \
     orient "[11-20]" "[0001]" "[-1100]" \
     -duplicate ${dup_x} ${dup_y} ${dup_z} \
-    -fix x -fix y \
-    -cell add ${vacuum} z \
-    -shift 0 0 ${vacuum_half} \
-    -sort species pack \
-    -fractional vasp
+    -cell add ${vacuum} z -shift 0 0 ${vacuum_half} \
+    -sort species pack -fractional vasp
 
   echo -e "\n"
   printf "%`tput cols`s" | tr ' ' '#'
   echo -e "\n"
 
-  mv POSCAR POSCAR.prism
+  mv POSCAR POSCAR.prismatic
 
+
+  #------------------------- PyramidalI / {10-11} 表面 ---------------------------
+  # 默认扩胞 1x1x2
+  dup_z=$(awk "BEGIN {print ${num_layer}/6}")
+  atomsk --create hcp ${a} ${c} ${symbol} \
+    orient "[-2110]" "[1-213]" "[01-11]" \
+    -duplicate ${dup_x} ${dup_y} ${dup_z} \
+    -cell add ${vacuum} z -shift 0 0 ${vacuum_half} \
+    -sort species pack -fractional vasp
+
+  echo -e "\n"
+  printf "%`tput cols`s" | tr ' ' '#'
+  echo -e "\n"
+
+  mv POSCAR POSCAR.pyramidalI
+
+
+  #------------------------- PyramidalII / {11-22} 表面 ---------------------------
+  # 默认扩胞 1x1x1
+  dup_z=$(awk "BEGIN {print ${num_layer}/12}")
+  atomsk --create hcp ${a} ${c} ${symbol} \
+    orient "[-1-123]" "[-1100]" "[11-22]" \
+    -duplicate ${dup_x} ${dup_y} ${dup_z} \
+    -cell add ${vacuum} z -shift 0 0 ${vacuum_half} \
+    -sort species pack -fractional vasp
+
+  echo -e "\n"
+  printf "%`tput cols`s" | tr ' ' '#'
+  echo -e "\n"
+
+  mv POSCAR POSCAR.pyramidalII
+
+
+  #------------------------- 创建存储结构文件的目录 ---------------------------
   structure_folder="hcp-surfaces"
   if [[ ! -d ${structure_folder} ]]; then
     mkdir ${structure_folder}
   fi
 
-  mv POSCAR.basal POSCAR.prism ${structure_folder}
+  mv POSCAR.basal POSCAR.prismatic POSCAR.pyramidalI POSCAR.pyramidalII ${structure_folder}
 
-  echo "Total Basal / (001), Prismatic / (100) surface models of HCP ${symbol} generated!"
+  echo "Total HCP Basal / {0001}, Prismatic / {11-20}, PyramidalI / {10-11}, PyramidalII / {11-22} surface models of HCP ${symbol} generated!"
 }
 
 
@@ -67,18 +97,18 @@ get_help() {
 
   echo -e "\nUsage: ${script_name} [-e symbol] [-lc lattice_constant] [-d dup_x dup_y num_layer] [-vac vacuum]"
 
-  echo -e "\nGenerate HCP Basal / (001), Prismatic / (100) surface models for VASP calculation."
+  echo -e "\nGenerate HCP Basal / {0001}, Prismatic / {11-20}, PyramidalI / {10-11}, PyramidalII / {11-22} surface models for VASP calculation."
 
   echo -e "\nOptions:"
   echo "    -h, --help                 show this help message and exit"
   echo "    -e element                 element symbol (default: Ti)"
   echo "    -lc lattice_constant       lattice constant (default: 2.928 4.640)"
   echo "    -d dup_x dup_y num_layer   x y dimension, layers of z direction (default: 1 1 12)"
-  echo "    -vac vacuum                vacuum thickness (default: 15)"
+  echo "    -vac vacuum                vacuum thickness (default: 15.0)"
 
   echo -e "\nExamples:"
   echo "    ${script_name}"
-  echo "    ${script_name} -e Ti -lc 2.928 4.640 -d 1 1 12 -vac 15"
+  echo "    ${script_name} -e Ti -lc 2.928 4.640 -d 1 1 12 -vac 15.0"
 }
 
 
