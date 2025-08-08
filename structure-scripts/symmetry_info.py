8af61731f61da082476ba082629cbf88698d9a89
BPY <3242809681@qq.com>
2025-08-08 10:20:02 +0800

    update symmetry_info.py

--- structure-scripts/symmetry_info.py
+++ structure-scripts/symmetry_info.py
@@ -3,7 +3,7 @@
 """
 获取结构的对称性信息
 
-已知问题: 对于 8g NbI、8g NbII、8g NbIII 的 Nb3Si 结构，程序将其统计成了 24g，是错误的
+已知问题: 对于 Ti3P 型 Nb3Si 结构，其 wyckoff 为 8g NbI、8g NbII、8g NbIII，程序将其统计成了 24g，是错误的
 解决方式: 添加 -v/--verbose 选项，进一步查看 Equivalent Site Group 信息进行检查
 
 reference: https://github.com/nanyanshouhu/Defect_generator/blob/main/wyckoff_position_finder.py
@@ -13,6 +13,7 @@ import argparse
 from collections import Counter
 
 from ase.io import read
+from ase.io.vasp import read_vasp
 from pymatgen.core import Structure
 from pymatgen.core.sites import PeriodicSite
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
@@ -26,13 +27,10 @@ def symmetry_info(
 ):
     """获取结构的对称性信息"""
 
-    atoms = read(structure_fn)
-    structure = Structure.from_ase_atoms(atoms)
+    structure = Structure.from_file(structure_fn)
 
     sga = SpacegroupAnalyzer(
-        structure=structure,
-        symprec=symprec,
-        angle_tolerance=angle_tolerance,
+        structure=structure, symprec=symprec, angle_tolerance=angle_tolerance
     )
 
     symmetry_dataset = sga.get_symmetry_dataset()
@@ -81,24 +79,14 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
-        "structure_fn",
-        type=str,
-        nargs="?",
-        default="POSCAR",
-        help="Structure filename",
+        "structure_fn", nargs="?", default="POSCAR", help="structure filename"
     )
 
     parser.add_argument(
-        "-v",
-        "--verbose",
-        action="store_true",
-        help="Show Equivalent Site Group info",
+        "-v", "--verbose", action="store_true", help="show equivalent site group info"
     )
 
     args = parser.parse_args()
 
     structure_fn = args.structure_fn
-    symmetry_info(
-        structure_fn=structure_fn,
-        verbose=args.verbose,
-    )
+    symmetry_info(structure_fn=structure_fn, verbose=args.verbose)
