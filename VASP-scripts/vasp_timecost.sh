889509cd1385adb7210b87689e71a2a746170534
yangsl <3242809681@qq.com>
2025-08-09 22:35:59 +0800

    .

--- VASP-scripts/vasp_timecost.sh
+++ VASP-scripts/vasp_timecost.sh
@@ -18,6 +18,6 @@ time_cost=$(grep 'LOOP+' "${outcar_fn}" | awk '{sum+=$7} END {print sum}')
 
 hour=$(echo "${time_cost} / 3600" | bc)
 minute=$(echo "${time_cost} % 3600 / 60" | bc)
-second=$(echo "${time_cost} % 60" | bc)
+second=$(echo "${time_cost} % 60" | bc | awk '{printf "%.0f\n", $1}')
 
 echo -e "\n${nsteps} inonic steps, current energy: ${energy} eV, time cost: ${hour}h ${minute}m ${second}s"
