449873cca9a401c67a3894fdcb5a67fa6321d7e6
yangsl <3242809681@qq.com>
2025-08-09 19:27:46 +0800

    update

--- atomate-usage/get_atomate_calc_info.py
+++ atomate-usage/get_atomate_calc_info.py
@@ -8,65 +8,76 @@ import os
 from pymatgen.io.vasp import Vasprun, Outcar
 
 
-def get_atomate_calc_info(root_dir: str):
+def get_atomate_calc_info(root_dir: str, flag: bool = False):
     """获取 atomate VASP 计算目录的计算信息"""
 
     os.chdir(root_dir)
 
-    dir_list = []
+    # 仅当 添加 --flag 时，才显示保存的 csv 文件
+    csv_fn = "atomate_calc_info.csv"
+    if os.path.exists(csv_fn) and flag:
+        df = pd.read_csv(csv_fn)
 
-    for dir_name in os.listdir("."):
-        if os.path.isdir(dir_name):
-            dir_list.append(dir_name)
+        print()
+        print(df)
+    else:
 
-    # 若计算完成，计算目录下的所有文件会被压缩
-    outcar_fn_list = ["OUTCAR", "OUTCAR.gz", "OUTCAR.relax2.gz"]
-    vasprun_fn_list = ["vasprun.xml", "vasprun.xml.gz", "vasprun.xml.relax2.gz"]
+        dir_list = []
 
-    data_list = []
-    for dir_name in dir_list:
-        for outcar_fn, vasprun_fn in zip(outcar_fn_list, vasprun_fn_list):
-            outcar_fn = os.path.join(dir_name, outcar_fn)
-            vasprun_fn = os.path.join(dir_name, vasprun_fn)
+        for dir_name in os.listdir("."):
+            if os.path.isdir(dir_name):
+                dir_list.append(dir_name)
 
-            for namefile in os.listdir(dir_name):
-                if "FW--" in namefile:
-                    if "gz" in namefile:
-                        namefile = namefile.replace(".gz", "")
-                    break
+        # 若计算完成，计算目录下的所有文件会被压缩
+        outcar_fn_list = ["OUTCAR", "OUTCAR.gz", "OUTCAR.relax2.gz"]
+        vasprun_fn_list = ["vasprun.xml", "vasprun.xml.gz", "vasprun.xml.relax2.gz"]
 
-            if os.path.exists(outcar_fn):
-                if "gz" in outcar_fn:
-                    outcar = Outcar(outcar_fn)
-                    time_cost = outcar.run_stats["Total CPU time used (sec)"]
+        data_list = []
+        for dir_name in dir_list:
+            for outcar_fn, vasprun_fn in zip(outcar_fn_list, vasprun_fn_list):
+                outcar_fn = os.path.join(dir_name, outcar_fn)
+                vasprun_fn = os.path.join(dir_name, vasprun_fn)
 
-                    vasprun = Vasprun(vasprun_fn)
-                    nsteps = int(vasprun.nionic_steps)
-                    energy = vasprun.final_energy
+                for namefile in os.listdir(dir_name):
+                    if "FW--" in namefile:
+                        if "gz" in namefile:
+                            namefile = namefile.replace(".gz", "")
+                        break
+
+                if os.path.exists(outcar_fn):
+                    if "gz" in outcar_fn:
+                        outcar = Outcar(outcar_fn)
+                        time_cost = outcar.run_stats["Total CPU time used (sec)"]
+
+                        vasprun = Vasprun(vasprun_fn)
+                        nsteps = int(vasprun.nionic_steps)
+                        energy = vasprun.final_energy
+                    else:
+                        time_cost = None
+                        nsteps = None
+                        energy = None
+
+                    data_dict = {
+                        "Ion_Step": nsteps,
+                        "Energy": energy,
+                        "Time_Cost": time_cost,
+                        "namefile": namefile,
+                    }
+                    data_list.append(data_dict)
+
                 else:
-                    time_cost = None
-                    nsteps = None
-                    energy = None
+                    continue
 
-                data_dict = {
-                    "Ion_Step": nsteps,
-                    "Energy": energy,
-                    "Time_Cost": time_cost,
-                    "namefile": namefile,
-                }
-                data_list.append(data_dict)
-
-            else:
-                continue
-
-    df = pd.DataFrame(data_list)
-    print()
-    print(df)
+        df = pd.DataFrame(data_list)
+        print()
+        print(df)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("root_dir", type=str, help="root directory")
+    parser.add_argument("--flag", action="store_true", help="show saved csv file")
+
     args = parser.parse_args()
 
-    get_atomate_calc_info(args.root_dir)
+    get_atomate_calc_info(args.root_dir, args.flag)
--- atomate-usage/get_atomate_calc_info.sh
+++ atomate-usage/get_atomate_calc_info.sh
@@ -5,8 +5,10 @@ parent_root_dir=$1
 
 # 若 parent... 为空，则 root_dir 为当前目录
 if [ -z "$parent_root_dir" ]; then
+    # 还有 atomate 计算在运行
     root_dir_list=$(squeue -u yangsl -t RUNNING --format "%.6i %.7T %.8j %.9N %.4C %.11M  %Z" | grep 'FW_job' | awk '{print $6}' | grep "yangsl")
 else
+    # 所有 atomate 计算已完成
     root_dir_list=$(ls -d $parent_root_dir/*)
 fi
 
@@ -14,7 +16,11 @@ fi
 for root_dir in $root_dir_list; do
     echo $root_dir | sed "s:/home/yangsl:~:g"
 
-    get_atomate_calc_info.py $root_dir
+    if [ -z "$parent_root_dir" ]; then
+        get_atomate_calc_info.py $root_dir
+    else
+        get_atomate_calc_info.py $root_dir --flag
+    fi
 
     echo -e "\n"
     printf "%`tput cols`s" | tr ' ' '#'
