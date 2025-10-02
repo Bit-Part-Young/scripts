89991c6310af2a222a16c84f1c62e10752c27579
yangsl <3242809681@qq.com>
2025-10-02 11:07:19 +0800

    update & add seelct frames scripts

--- NEP/select_xyz_frames2.py
+++ NEP/select_xyz_frames2.py
@@ -1,43 +1,48 @@
 #!/usr/bin/env python3
 
-"""按比例选取指定个数的小于受力阈值的构型（用于缺陷、层错构型弛豫轨迹构型的筛选）"""
+"""
+按比例选取指定个数的小于受力阈值的构型（用于缺陷、层错构型弛豫轨迹构型的筛选）
+需借助 plot_energy_force.sh 脚本生成 force_info.dat 文件
+"""
 
 import argparse
 
 import numpy as np
 
 
-def parse_xyz_file(filename: str) -> list:
+def parse_xyz_file(input_xyz_fn: str) -> list:
     """解析 extxyz 文件"""
 
-    with open(filename, "r") as file:
+    with open(input_xyz_fn, "r") as file:
         lines = file.readlines()
 
     frames = []
     i = 0
     while i < len(lines):
-        num_atoms = int(lines[i].strip())
+        natoms = int(lines[i].strip())
         frame_info = lines[i + 1].strip()
 
-        atoms = lines[i + 2 : i + 2 + num_atoms]
-        frames.append((num_atoms, frame_info, atoms))
-        i += 2 + num_atoms
+        atoms_info = lines[i + 2 : i + 2 + natoms]
+        frames.append((natoms, frame_info, atoms_info))
+        i += 2 + natoms
 
     return frames
 
 
-def write_xyz_file(frames: list, output_filename: str):
+def write_xyz_file(frames: list, output_xyz_fn: str):
     """写入 extxyz 文件"""
 
-    with open(output_filename, "w") as file:
-        for num_atoms, frame_info, atoms in frames:
-            file.write(f"{num_atoms}\n")
+    with open(output_xyz_fn, "w") as file:
+        for natoms, frame_info, atoms_info in frames:
+            file.write(f"{natoms}\n")
             file.write(f"{frame_info}\n")
-            for atom in atoms:
-                file.write(f"{atom.strip()}\n")
+            for atom_info in atoms_info:
+                file.write(f"{atom_info.strip()}\n")
 
 
-def force_exceeds_threshold(threshold: float = 5.0, num_selected: int = 5) -> list:
+def force_exceeds_threshold(
+    force_threshold: float = 5.0, num_selected: int = 5
+) -> list:
     """按比例选取指定个数的小于受力阈值的构型索引"""
 
     force = np.loadtxt("force_info.dat", skiprows=1)
@@ -46,51 +51,47 @@ def force_exceeds_threshold(threshold: float = 5.0, num_selected: int = 5) -> li
         force = np.atleast_1d(force)
 
     # 按比例选取 N 个小于受力阈值的构型索引；若构型索引数小于 N，则返回所有构型索引
-    index = np.where(force < threshold)[0]
-    if len(index) < num_selected:
-        final_index = index.tolist()
+    indices = np.where(force < force_threshold)[0]
+    if len(indices) < num_selected:
+        final_indices = indices.tolist()
     else:
-        index_selected = np.linspace(0, len(index) - 1, num_selected, dtype=int)
-        final_index = index[index_selected].tolist()
+        indices_selected = np.linspace(0, len(indices) - 1, num_selected, dtype=int)
+        final_indices = indices[indices_selected].tolist()
 
-    return final_index
+    return final_indices
 
 
 def filter_frames(frames: list, force_threshold: float, num_selected: int = 5):
     """筛选构型"""
 
-    index = force_exceeds_threshold(force_threshold, num_selected)
+    indices = force_exceeds_threshold(force_threshold, num_selected)
 
-    filtered_frames = [frames[i] for i in index]
+    filtered_frames = [frames[i] for i in indices]
 
-    removed_frames = [frames[i] for i in range(len(frames)) if i not in index]
-    removed_frames_indices = [i + 1 for i in range(len(frames)) if i not in index]
+    removed_frames = [frames[i] for i in range(len(frames)) if i not in indices]
+    removed_frames_indices = [i + 1 for i in range(len(frames)) if i not in indices]
 
     return filtered_frames, removed_frames, removed_frames_indices
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Select/Filter frames from exyz configuration file according to the force threshold.",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description="Select/Filter frames from extxyz file according to the force threshold.",
         epilog="Author: SLY.",
     )
 
     parser.add_argument(
-        "input_filename",
-        type=str,
+        "input_xyz_fn",
         default="train.xyz",
         help="input xyz filename (default: train.xyz)",
     )
     parser.add_argument(
-        "output_filename",
-        type=str,
+        "output_xyz_fn",
         default="train_filtered.xyz",
         help="output xyz filename (default: train_filtered.xyz)",
     )
     parser.add_argument(
-        "removed_filename",
-        type=str,
+        "removed_xyz_fn",
         default="train_removed.xyz",
         help="removed xyz filename (default: train_removed.xyz)",
     )
@@ -113,21 +114,21 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    input_filename = args.input_filename
-    output_filename = args.output_filename
-    removed_filename = args.removed_filename
+    input_xyz_fn = args.input_xyz_fn
+    output_xyz_fn = args.output_xyz_fn
+    removed_xyz_fn = args.removed_xyz_fn
     force_threshold = args.force_threshold
     num_selected = args.num_selected
 
-    frames = parse_xyz_file(input_filename)
+    frames = parse_xyz_file(input_xyz_fn)
     filtered_frames, removed_frames, removed_frames_indices = filter_frames(
         frames, force_threshold, num_selected
     )
-    write_xyz_file(filtered_frames, output_filename)
-    write_xyz_file(removed_frames, removed_filename)
+    write_xyz_file(filtered_frames, output_xyz_fn)
+    write_xyz_file(removed_frames, removed_xyz_fn)
 
     print(f"\nRemoved frame indices (starting from 1): {removed_frames_indices}.")
 
     print(
-        f"\nFiltered structures saved to {output_filename}, removed structures saved to {removed_filename}."
+        f"\nFiltered structures saved to {output_xyz_fn}, removed structures saved to {removed_xyz_fn}."
     )
--- NEP/select_xyz_frames3.py
+++ NEP/select_xyz_frames3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-根据原子受力阈值筛选 extxyz 文件中的构型
+筛选原子受力均为 0.0 的构型
 
 reference: https://github.com/brucefan1983/GPUMD/blob/master/tools/Analysis_and_Processing/select_xyz_frames/select_xyz_frames.py
 """
@@ -46,8 +46,8 @@ def write_xyz_file(frames: list, output_xyz_fn: str):
                 f.write(f"{atom_info.strip()}\n")
 
 
-def force_exceeds_threshold(atom_info: str, force_threshold: float):
-    """判断原子受力是否超过阈值"""
+def force_exceeds_threshold(atom_info: str):
+    """判断原子受力是否 0.0"""
 
     atom_info_list = atom_info.split()
     if len(atom_info_list) == 7:
@@ -60,10 +60,10 @@ def force_exceeds_threshold(atom_info: str, force_threshold: float):
     # 合力
     force_norm = np.linalg.norm(force_vector)
 
-    return force_norm > force_threshold
+    return np.isclose(force_norm, 0.0)
 
 
-def filter_frames(frames: list, force_threshold: float) -> tuple[list, list, list]:
+def filter_frames(frames: list) -> tuple[list, list, list]:
     """筛选构型"""
 
     filtered_frames = []
@@ -74,11 +74,8 @@ def filter_frames(frames: list, force_threshold: float) -> tuple[list, list, lis
         current_frame = frames[i]
         natoms, frame_info, energy, lattice, atoms_info = current_frame
 
-        # 如果力超过阈值，则删除该帧对应的构型
-        if any(
-            force_exceeds_threshold(atom_info, force_threshold)
-            for atom_info in atoms_info
-        ):
+        # 如果原子受力均为 0.0，则删除该帧对应的构型
+        if all(force_exceeds_threshold(atom_info) for atom_info in atoms_info):
             # 记录被删除的构型索引，从 1 开始
             removed_frames_indices.append(i + 1)
             removed_frames.append(current_frame)
@@ -92,37 +89,29 @@ def filter_frames(frames: list, force_threshold: float) -> tuple[list, list, lis
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Select/Filter frames from extxyz file according to the force threshold.",
+        description="Select/Filter frames of atoms with zero force from extxyz file.",
     )
 
     parser.add_argument(
         "input_xyz_fn",
+        type=str,
         default="train.xyz",
         help="input xyz filename (default: train.xyz)",
     )
     parser.add_argument(
         "output_xyz_fn",
+        type=str,
         default="train_filtered.xyz",
         help="output xyz filename (default: train_filtered.xyz)",
     )
-    parser.add_argument(
-        "-f",
-        "--force_threshold",
-        type=float,
-        default=50.0,
-        metavar="FLOAT",
-        help="force threshold",
-    )
+
     args = parser.parse_args()
 
     input_xyz_fn = args.input_xyz_fn
     output_xyz_fn = args.output_xyz_fn
-    force_threshold = args.force_threshold
 
     frames = parse_xyz_file(input_xyz_fn)
-    filtered_frames, removed_frames, removed_frames_indices = filter_frames(
-        frames, force_threshold
-    )
+    filtered_frames, removed_frames, removed_frames_indices = filter_frames(frames)
     write_xyz_file(filtered_frames, output_xyz_fn)
     write_xyz_file(removed_frames, "removed.xyz")
 
