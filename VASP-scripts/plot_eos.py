e131580f5e4ab5387c6758f4a685c22bdc7d4800
YSL <3242809681@qq.com>
2025-10-02 22:54:03 +0800

    update

--- VASP-scripts/plot_eos.py
+++ VASP-scripts/plot_eos.py
@@ -6,6 +6,8 @@ import argparse
 
 import matplotlib.pyplot as plt
 import pandas as pd
+from matplotlib.ticker import MultipleLocator
+
 from spt.plot_params import set_roman_plot_params
 
 
@@ -23,15 +25,22 @@ def plot_eos(
         df_volume = df.iloc[:, 0]
         df_energy = df.iloc[:, 1]
 
-    set_roman_plot_params()
-    fig, ax = plt.subplots(figsize=(6, 6))
+    set_roman_plot_params(lines_linewidth=3.0)
+    fig, ax = plt.subplots(figsize=(8, 6))
 
-    ax.plot(df_volume, df_energy, "o-", linewidth=3.0, markerfacecolor="none")
+    ax.plot(df_volume, df_energy, "o-", markerfacecolor="none", label="EOS")
+
+    ax.legend()
+
+    ax.set_xlim(int(df_volume.min()), int(df_volume.max()) + 1.1)
+
+    ax.xaxis.set_minor_locator(MultipleLocator(1.0))
 
     ax.set_xlabel("Volume ($\AA^3$/atom)")
     if not deltaE:
         ax.set_ylabel("Energy (eV/atom)")
     else:
+        ax.yaxis.set_minor_locator(MultipleLocator(0.05))
         ax.set_ylabel(f"$\Delta$E (eV/atom)")
 
     fig.savefig(figure_fn)
--- VASP-scripts/plot_eos_multiple.py
+++ VASP-scripts/plot_eos_multiple.py
@@ -6,6 +6,8 @@ import argparse
 
 import matplotlib.pyplot as plt
 import pandas as pd
+from matplotlib.ticker import MultipleLocator
+
 from spt.plot_params import set_roman_plot_params
 
 
@@ -14,7 +16,7 @@ def plot_eos_multiple(
 ):
     """EOS 曲线多条绘制"""
 
-    set_roman_plot_params()
+    set_roman_plot_params(lines_linewidth=3.0)
     fig, ax = plt.subplots(figsize=(8, 6))
 
     linestyle_list = ["o-", "s-", "D-", "p-", "x-", "v-"]
@@ -30,18 +32,20 @@ def plot_eos_multiple(
             df_energy = df.iloc[:, 1]
 
         ax.plot(
-            df_volume,
-            df_energy,
-            linestyle_list[i],
-            linewidth=3.0,
-            markerfacecolor="none",
-            label=label,
+            df_volume, df_energy, linestyle_list[i], markerfacecolor="none", label=label
         )
+
+    ax.legend()
+
+    ax.set_xlim(int(df_volume.min()), int(df_volume.max()) + 1.1)
+
+    ax.xaxis.set_minor_locator(MultipleLocator(1.0))
 
     ax.set_xlabel("Volume ($\AA^3$/atom)")
     if not deltaE:
         ax.set_ylabel("Energy (eV/atom)")
     else:
+        ax.yaxis.set_minor_locator(MultipleLocator(0.05))
         ax.set_ylabel(f"$\Delta$E (eV/atom)")
 
     fig.savefig(figure_fn)
