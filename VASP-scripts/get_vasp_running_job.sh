fb0e80cb59ce947eddfd14472a3ca46bb4bb2d31
yangsl <3242809681@qq.com>
2025-11-18 15:36:52 +0800

    update

--- VASP-scripts/get_vasp_running_job.sh
+++ VASP-scripts/get_vasp_running_job.sh
@@ -2,14 +2,20 @@
 
 set -eu
 
-# 获取正在运行的 VASP 作业信息
+# 获取正在运行的 VASP 作业信息（scf 和 opt 类型）
 
-type=$1
+cal_type=$1
 
-root_dir_list=($(squeue -u yangsl -t RUNNING --format "%.6i %.7T %.8j %.9N %.4C %.11M  %Z" | grep 'VASP' | awk '{print $7}' | grep "yangsl"))
+platform=$(hostname)
 
-if [ "$type" == "scf" ]; then
-    get_scf_data.sh ${root_dir_list[@]}
-elif [ "$type" == "opt" ]; then
-    get_opt_data.sh ${root_dir_list[@]}
+if [[ ${platform} =~ "sjtu" ]]; then
+    dir_list=($(squeue -t RUNNING --format "%.6i %.7T %.8j %.9N %.4C %.11M  %Z" | grep 'VASP' | awk '{print $7}' | grep "yangsl"))
+else
+    dir_list=($(squeue -u yangsl -t RUNNING --format "%.6i %.7T %.8j %.9N %.4C %.11M  %Z" | grep 'VASP' | awk '{print $7}' | grep "yangsl"))
+fi
+
+if [ "$cal_type" == "scf" ]; then
+    get_scf_data.sh ${dir_list[@]}
+elif [ "$cal_type" == "opt" ]; then
+    get_opt_data.sh ${dir_list[@]}
 fi
